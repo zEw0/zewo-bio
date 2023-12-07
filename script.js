8349826b0e531c840795db0a8e9b15937e549f8d
unknown <rainyboy920@gmail.com>
2023-12-08 02:39:30 +0800

    first idk

---
+++ script.js
@@ -0,0 +1,57 @@
+window.ondataload = () => { }
+fetch("https://api.lanyard.rest/v1/users/465691254332719104").then((a) => a.json().then((a) => {
+    window.lanyard = a.data
+    window.ondataload()
+
+    if (window.lanyard.discord_status != "offline") {
+        stattext.innerText = "Online"
+        document.getElementById("stattext").style.color='#0BC544'
+        document.getElementById("statsymbol").style.backgroundColor='#0BC544'
+        }
+
+        const imagepfp = `https://cdn.discordapp.com/avatars/465691254332719104/${window.lanyard.discord_user.avatar}`
+        document.getElementById("pfp").style.backgroundImage= `url(${imagepfp})`
+
+}))
+
+/*if(window.lanyard.discord_status = "dnd"){
+    stattext.innerText = "Online"
+    document.getElementById("stattext").style.color='#AB0909'*/
+
+    function pagetitle(){
+        var Flip = false;
+
+	        var CurStr = "";
+	        var Pos = 0;
+	        var Invert = false;
+
+	        var TextL = "zewo.bio";
+	        var TextR = "zewo.bio";
+
+	        function loop() {
+	            var Target = Flip ? TextR : TextL;
+
+	            if (Pos > Target.length) {
+	                Invert = true;
+	            } else if (Pos < 0) {
+	                if (Invert) {
+	                    Flip = !Flip;
+	                    Target = Flip ? TextR : TextL;
+
+	                    Pos = 0;
+	                }
+
+	                Invert = false;
+	            }
+
+	            CurStr = Target.substring(0, Pos);
+	            Pos += (Invert ? -1 : 1);
+
+	            if (CurStr.length == 0)
+	                CurStr = "k";
+
+	            document.getElementsByTagName("title")[0].innerHTML = CurStr;
+	        }
+
+	        setInterval(loop, 300);
+    }
